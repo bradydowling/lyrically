69d8b5c8acd74589a4bb52f0e385b0e9cdda6602
Brady Dowling <bradydowling@gmail.com>
2018-12-13 21:08:47 -0800

    Add character and word count

--- index.py
+++ index.py
@@ -1,7 +1,10 @@
 import lyricsgenius as genius
 with open(".genius_key", "r") as myfile:
     genius_key = myfile.read().replace("\n", "")
+
 api = genius.Genius(genius_key)
-artist = api.search_artist("Logic", max_songs=3)
-song = api.search_song("The Glorious Five", artist.name)
+song = api.search_song("The Glorious Five", "Logic")
+print("Lyrics")
 print(song.lyrics)
+print("Total characters: ", len(song.lyrics))
+print("Word count: ", len(song.lyrics.split()))
