c780059bd72dea4ddfe96e181636589c5cb7ede7
Brady Dowling <bradydowling@gmail.com>
2018-12-17 18:41:46 -0800

    Start cleaning lyrics

--- index.py
+++ index.py
@@ -2,13 +2,17 @@ import lyricsgenius as genius
 with open(".genius_key", "r") as myfile:
     genius_key = myfile.read().replace("\n", "")
 
+def cleanLyrics(lyrics):
+    cleanedLyrics = lyrics.replace("Fuck", "F***")
+    cleanedLyrics = lyrics.replace("fuck", "f***")
+    cleanedLyrics = cleanedLyrics.replace("Shit", "S***")
+    cleanedLyrics = cleanedLyrics.replace("shit", "s***")
+    return cleanedLyrics
+
 api = genius.Genius(genius_key)
 song = api.search_song("The Glorious Five", "Logic")
 print("Lyrics")
-print(song.lyrics)
+print(cleanLyrics(song.lyrics))
 print("Total characters: ", len(song.lyrics))
 words = song.lyrics.split()
 print("Word count: ", len(words))
-
-def censorLyrics(lyricsArray):
-    for word in lyricsArray
