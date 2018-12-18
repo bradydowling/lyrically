467be8d466e79de9f26ad6449f43204e0632fac6
Brady Dowling <bradydowling@gmail.com>
2018-12-17 22:31:53 -0800

    Add word frequency counter

--- index.py
+++ index.py
@@ -9,10 +9,25 @@ def getLyricsStats(lyrics):
     stats = {"charsNum": characters, "wordsNum": words}
     return stats
 
+def getWordFrequency(lyrics):
+    # need to sort and cleanup this dict
+    wordsMap = dict()
+    for word in lyrics:
+        if word in wordsMap:
+            wordsMap[word] += 1
+        else:
+            wordsMap[word] = 1
+    return wordsMap
+
 api = genius.Genius(genius_key)
 song = api.search_song("The Glorious Five", "Logic")
+
 print("Lyrics")
 print(cleanLyrics.censor(song.lyrics))
+
 lyricsStats = getLyricsStats(song.lyrics)
 print("Total characters: ", lyricsStats["charsNum"])
 print("Word count: ", lyricsStats["wordsNum"])
+lyricWords = song.lyrics.split()
+
+print("Word frequency: ", getWordFrequency(lyricWords))
